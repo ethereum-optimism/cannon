73bb0fdd9f28e7256868144f92e83de6c8948127
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2021-10-11 15:56:13 +0200

    Adjust function naming in tests.

--- test/libkeccak.js
+++ test/libkeccak.js
@@ -9,18 +9,18 @@ describe("MIPSMemory contract", function () {
     const mm = await MIPSMemory.deploy();
     console.log("deployed at", mm.address, "by", owner.address);
 
-    await mm.AddLargePreimageInit();
+    await mm.addLargePreimageInit();
     console.log("preimage initted");
 
     // block size is 136
     const a = ["0x0100000000000000",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0x80];
-    await mm.AddLargePreimageUpdate(a);
+    await mm.addLargePreimageUpdate(a);
     console.log("preimage updated");
 
     /*var tst1 = await mm.largePreimage(owner.address, 0);
     console.log(tst);*/
 
-    const hash = await mm.AddLargePreimageFinal();
+    const hash = await mm.addLargePreimageFinal();
     const realhash = keccak256(new Uint8Array(0));
     console.log("comp hash is", hash);
     console.log("real hash is", realhash);
